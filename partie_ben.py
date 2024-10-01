9e6ccf0e045f9b400c3bdc90aa314ea15e4c271d
Benjamin <benjamin.labonne.03@outlook.fr>
2024-10-01 13:53:49 +0200

    tout sauf 14

--- partie_ben.py
+++ partie_ben.py
@@ -11,9 +11,26 @@ def probabilites_phrase(phrase):
     #Calcul de la probabilité :
     for k in cptdict :
         res+=(cptdict[k]/l)*((cptdict[k]-1)/(l-1))
-    print(res)
+    print("K = ",res)
+    return res
 
+def friedman(cypher):
+    Kr = 1/26 #Random distribution
+    Ke = 0.067 #English Language
+    K = probabilites_phrase(cypher)
+    L = (Ke-Kr)/(K-Kr)
+    print("L = ",L)
 
 probabilites_phrase("bonjourjesuisunephraseenminusculesansespaces")
 probabilites_phrase("cameo")
 probabilites_phrase("pepe")
+
+print("Test de friedman 1: ")
+# m=helloiambenjaminilovechocolate
+# c=uwu
+friedman("baffkcuivyjduichefirywdiwkfupy")
+
+print("Test de friedman 2: ")
+# m = helloiamanenglishsentencewithalotofwordsinitandeventhoughmynameisbenjaminiamold
+# c=c
+friedman("jgnnqkcocpgpinkujugpvgpegykvjcnqvqhyqtfukpkvcpfgxgpvjqwijoapcogkudgplcokpkcoqnf")
